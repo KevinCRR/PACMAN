948d07191cf8fbe0e6c864c1a6304c0761c5f09a
Kevin Romero <kevin.christopher13@gmail.com>
2020-04-06 19:23:13 -0400

    Collision

--- PacMan/Assets/C# Scripts/Movement.cs
+++ PacMan/Assets/C# Scripts/Movement.cs
@@ -9,10 +9,21 @@ public class Movement : MonoBehaviour
 
     private Vector2 pacmanDirection = Vector2.zero;
 
+    //private Node currentNode;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.localPosition = new Vector3(13, -22.5f,1);
+
+        //Node node = GetNodeAtPosition(transform.localPosition);
+        //if (node != null)
+        //{
+        //    currentNode = node;
+        //    Debug.Log(currentNode);
+        //}
+
+        transform.localPosition = new Vector3(13, -22.5f, 1);
+
 
     }
 
@@ -23,7 +34,7 @@ public class Movement : MonoBehaviour
         CheckInput();
 
         UpdateOrientation();
-        Debug.Log(transform.position.x);
+        ///Debug.Log(transform.position.x);
 
         if (transform.position.x >= 27f)
         {
@@ -44,7 +55,7 @@ public class Movement : MonoBehaviour
         }
         Move();
 
-        
+
     }
 
     // Check the current player's input
@@ -52,18 +63,22 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             pacmanDirection = Vector2.left;
+            //MoveToNode(pacmanDirection);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             pacmanDirection = Vector2.right;
+            //MoveToNode(pacmanDirection);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             pacmanDirection = Vector2.down;
+            //MoveToNode(pacmanDirection);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             pacmanDirection = Vector2.up;
+            //MoveToNode(pacmanDirection);
         }
     }
 
@@ -72,6 +87,18 @@ public class Movement : MonoBehaviour
     {
         transform.localPosition += (Vector3)(pacmanDirection * pacmanSpeed) * Time.deltaTime;
     }
+
+
+    //void MoveToNode (Vector2 d)
+    //{
+    //    //Node moveToNode = CanMove(d);
+
+    //    if (moveToNode != null)
+    //    {
+    //        transform.localPosition = moveToNode.transform.position;
+    //        currentNode = moveToNode;
+    //    }
+    //}
 
     void UpdateOrientation()
     {
@@ -96,4 +123,30 @@ public class Movement : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
+
+    //Node CanMove (Vector2 d)
+    //{
+    //    Node moveToNode = null;
+        
+    //    for (int i = 0; i < currentNode.neighbours.Length; i++)
+    //    {
+    //        if (currentNode.validDirections [i] == d)
+    //        {
+    //            moveToNode = currentNode.neighbours[i];
+    //            break;
+    //        }
+    //    }
+    //    return moveToNode;
+    //}
+
+    //Node GetNodeAtPosition (Vector2 pos)
+    //{
+    //    GameObject tile = GameObject.Find("Game").GetComponent<GameBoard>().board[(int)pos.x, (int)pos.y];
+
+    //    if (tile != null)
+    //    {
+    //        return tile.GetComponent<Node>();
+    //    }
+    //    return null;
+    //}
 }
