f1f147baa88187d3db38ba1fbca09700d918bbd2
Aaron Chambers <aaron.chambers79@gmail.com>
2020-04-17 17:35:14 -0400

    Added button controls

---
+++ PacMan/Assets/C# Scripts/ButtonControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    public Button myBtn;
+    // Use this for initialization
+    void Start()
+    {
+        if (myBtn.name == "StartButton")
+        {
+            myBtn.onClick.AddListener(StartGame);
+        }
+        else
+        {
+            myBtn.onClick.AddListener(QuitGame);
+        }
+    }
+    void StartGame()
+    {
+        SceneManager.LoadScene("SampleScreen");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
